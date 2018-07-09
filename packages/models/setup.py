89a091f65353863d0b857fc5eb9b2d7ca817063a
Sergey Emelyanov <karagabul@gmail.com>
2018-07-09 19:09:05 +0300

    Add base exporter

---
+++ packages/models/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+install_requires = [
+    "pony>=0.7.3",
+]
+
+setup(
+    name='Project procurement',
+    version='0.1.0',
+    description='Project procurement models',
+    author='Sergey Emelyanov',
+    author_email='karagabul@gmail.com',
+    packages=[],
+    install_requires=install_requires
+ )
